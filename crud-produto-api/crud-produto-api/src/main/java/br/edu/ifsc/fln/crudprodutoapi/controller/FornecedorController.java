b3b4aa02675e10a4e394c4d64b88dccea6c7cea9
vinicarpes <vinicdf000@gmail.com>
2025-05-28 16:51:32 -0300

    novo endpoint para fornecedor com programação funcional

--- crud-produto-api/crud-produto-api/src/main/java/br/edu/ifsc/fln/crudprodutoapi/controller/FornecedorController.java
+++ crud-produto-api/crud-produto-api/src/main/java/br/edu/ifsc/fln/crudprodutoapi/controller/FornecedorController.java
@@ -2,11 +2,14 @@ package br.edu.ifsc.fln.crudprodutoapi.controller;
 
 import br.edu.ifsc.fln.crudprodutoapi.domain.Fornecedor;
 import br.edu.ifsc.fln.crudprodutoapi.repository.FornecedorRepository;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/fornecedores")
@@ -19,5 +22,10 @@ public class FornecedorController {
     @GetMapping()
     public List<Fornecedor> get(){
         return fornecedorRepository.findAll();
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<Fornecedor> get(@PathVariable int id){
+        Optional<Fornecedor> fornecedor = fornecedorRepository.findById(id);
+        return fornecedor.map(value -> ResponseEntity.ok().body(value)).orElseGet(() -> ResponseEntity.notFound().build());
     }
 }
